80a9c48bf5d3c48dc030960e61c5a0952a7f959d
filiplazovic <lazoviccorp@gmail.com>
2019-03-13 16:56:33 +0100

    Fixed link to angel list

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,6 @@ export default () => (
     <div className="footer">
         <a target="_blank" href="mailto:lazoviccorp@gmail.com" className="contact">contact</a>
         <a target="_blank" href="https://medium.com/genhub" className="contact">blog</a>
-        <a target="_blank" href="https://angel.co/genhub/jobs" className="contact">jobs</a>
+        <a target="_blank" href="https://angel.co/genhubco/jobs" className="contact">jobs</a>
     </div>
 )
