55f6d2c109f9b1967265c4b3a3de89510e52a493
filiplazovic <lazoviccorp@gmail.com>
2019-03-14 15:48:07 +0100

    New brand again

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { withRouter } from 'next/router'
 export default withRouter(({router}) => (
     <div className="header">
         <Link href="/">
-            <a className="logo"><img src="/static/logo.svg"/></a>
+            <a className="logo"><img src="/static/applogo.svg"/></a>
         </Link>
         {router.route !== "/demo" && <Link href="/demo">
             <button className="btn-primary">
