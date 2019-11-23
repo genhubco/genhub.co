82e9ce571df49d4063df69338e589ab55a8045f4
Filip Lazovic <filiplazovic@Filips-MacBook-Pro.local>
2019-11-23 02:30:20 +0100

    a

--- components/Header.js
+++ components/Header.js
@@ -3,74 +3,45 @@ import classnames from "classnames";
 import { withRouter } from "next/router";
 
 export default withRouter(({ router, user }) => {
-    return (
-        <div className="header">
-            <Link href="/">
-                <a className="header-logo"><img src="/static/applogo.svg"/></a>
-            </Link>
-            <div className="header-links">
-                <Link href="/docs?project=api&doc=auth">
-                    <a className={classnames("internal-link", {
-                        "internal-link-active": router.route === "/docs"
-                    })}>
-                        docs
-                    </a>
-                </Link>
-                <Link href="/demo">
-                    <a className={classnames("internal-link", {
-                        "internal-link-active": router.route === "/demo"
-                    })}>
-                        demo
-                    </a>
-                </Link>
-                {
-                    user && user.id ?
-                    <Link href={`/profile-projects?id=${user.id}`}>
-                        <a className="header-avatar-link">
-                            <img src={`${process.env.AVATAR_URL}?id=${user.email_sha256}&size=20`} />
-                        </a>
-                    </Link> :
-                    <Link href="/login">
-                        <a className={classnames("internal-link", {
-                            "internal-link-active": router.route === "/login"
-                        })}>
-                            login
-                        </a>
-                    </Link>
-                }
-            </div>
-            <style jsx global>{`
-                .header {
-                    padding-top: 10px;
-                    width: 100%;
-                    display: flex;
-                    justify-content: space-between;
-                }
+  return (
+    <div className="header">
+      <Link href="/">
+        <a className="header-logo">
+          <img src="/static/applogo.svg" />
+        </a>
+      </Link>
+      <style jsx global>{`
+        .header {
+          padding-top: 10px;
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+        }
 
-                .header-logo {
-                    height: 30px;
-                    margin-left: 25px;
-                }
+        .header-logo {
+          height: 30px;
+          margin-left: 25px;
+        }
 
-                .header-logo img {
-                    height: 30px;
-                }
+        .header-logo img {
+          height: 30px;
+        }
 
-                .header-avatar-link {
-                    display: inline-block;
-                    height: 20px;
-                    vertical-align: middle;
-                }
+        .header-avatar-link {
+          display: inline-block;
+          height: 20px;
+          vertical-align: middle;
+        }
 
-                .header-avatar-link img {
-                    border-radius: 50%;
-                }
+        .header-avatar-link img {
+          border-radius: 50%;
+        }
 
-                .header-links {
-                    padding-top: 5px;
-                    margin-right: 25px;
-                }
-            `}</style>
-        </div>
-    );
+        .header-links {
+          padding-top: 5px;
+          margin-right: 25px;
+        }
+      `}</style>
+    </div>
+  );
 });
--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-const api = "https://api-kpc9q1dny.now.sh/";
-const data = "https://data-o6m8znu9c.now.sh/";
-
 module.exports = {
-    target: "serverless",
-    env: {
-        TOKEN_COOKIE_NAME: "jwt-token-genhub-h4t98g4987tfg8944ea",
-        RESULT_IMAGES_NOW_NAME: "images-genhub-h4t98g4987tfg8944ea",
-        LOGIN_URL: api + "login",
-        PROFILE_URL: api + "profile",
-        PROJECTS_URL: api + "projects",
-        RESULTS_URL: api + "results",
-        AVATAR_URL: api + "avatar",
-        API_DOCS_URL: api + "docs/",
-        DATA_DOCS_URL: data + "docs/",
-        SEARCH_URL: data + "search",
-        SCORE_URL: data + "score",
-        PROXY_REDIRECT_URL: "https://proxy.genhub.now.sh/redirect",
-        GOOGLE_CLIENT_ID: "862067484080-upmf5o3qh3bmaeq7loub8imclm0ceebf.apps.googleusercontent.com",
-        GITHUB_CLIENT_ID: "21e31307f46eb37b341b",
-        ZEIT_API_TOKEN: process.env.ZEIT_API_TOKEN
-    }
+  target: "serverless"
 };
