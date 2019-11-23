ec7f787c5b5d31cc795bc73bf75fe1da185ca652
Filip Lazovic <filiplazovic@Filips-MacBook-Pro.local>
2019-11-23 02:36:43 +0100

    asd

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,26 @@
 export default () => (
-    <div className="footer">
-        <a target="_blank" href="mailto:lazoviccorp@gmail.com" className="link">contact</a>
-        <a target="_blank" href="https://medium.com/genhub" className="link">blog</a>
-        <a target="_blank" href="https://angel.co/genhubco/jobs" className="link">jobs</a>
-        <style jsx global>{`
-            .footer {
-                border-top: 1px solid #f2f3f4;
-                padding-top: 10px;
-                padding-bottom: 10px;
-                text-align: center;
-            }
-        `}</style>
-    </div>
-)
+  <div className="footer">
+    <a target="_blank" href="mailto:lazoviccorp@gmail.com" className="link">
+      {" "}
+      contact{" "}
+    </a>
+    -
+    <a target="_blank" href="https://medium.com/genhub" className="link">
+      {" "}
+      blog{" "}
+    </a>
+    -
+    <a target="_blank" href="https://angel.co/genhubco/jobs" className="link">
+      {" "}
+      jobs{" "}
+    </a>
+    <style jsx global>{`
+      .footer {
+        border-top: 1px solid #f2f3f4;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        text-align: center;
+      }
+    `}</style>
+  </div>
+);
