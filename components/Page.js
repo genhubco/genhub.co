b2b7aee0f62134ad992e87b0cdec259f95104eb9
Filip Lazovic <filiplazovic@Filips-MacBook-Pro.local>
2019-12-17 22:31:36 +0100

    Twitter card

--- components/Page.js
+++ components/Page.js
@@ -8,7 +8,7 @@ import css from "styled-jsx/css";
 
 const Page = ({
 	type = "normal",
-	title = "GenHub - Democratizing genetic engineering",
+	title = "Democratizing genetic engineering - GenHub",
 	children,
 }) => (
 	<div className="page">
@@ -16,6 +16,23 @@ const Page = ({
 			<title>{title}</title>
 			<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet"/>
 			<link rel="icon" type="image/png" href="/favicon.png" />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@gogenhub" />
+			<meta name="twitter:creator" content="@gogenhub" />
+			<meta
+				name="og:description"
+				content="Search for targets with highest score and review off-targets."
+			/>
+			<meta name="og:title" content="Democratizing genetic engineering - GenHub" />
+			<meta
+				name="og:image"
+				content={
+					process.env.NOW_GITHUB_COMMIT_REF === "staging" ?
+						"https://genhubco-git-staging.genhub.now.sh/twitter-card.png" :
+						"https://genhubco-git-master.genhub.now.sh/twitter-card.png"
+				}
+			/>
 		</Head>
 		<Header/>
 		<div className={classnames({
