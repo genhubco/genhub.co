17007b2815c30ad8aa6bf31ac85c0e0826e1ca3e
Filip <lazoviccorp@gmail.com>
2020-04-16 19:43:28 +0200

    Updated next, fixed domain ext

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 		FUNCTIONS: `${
 			process.env.NODE_ENV === "development" ?
 				"http://localhost:34567/.netlify/functions" :
-				"https://5e77c1ee4fee5d529b561188--api-genhub.netlify.com/.netlify/functions"
-		}`,
+				"https://5e77c1ee4fee5d529b561188--api-genhub.netlify.app/.netlify/functions"
+			}`,
 	},
 };
