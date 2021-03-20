07502162edf61b1383b6a83f06c5788ea075ccb8
Filip <lazoviccorp@gmail.com>
2021-03-20 20:52:09 +0100

    Add init

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,2 @@
-if [[ $(cargo --version) ]]; then
-    echo "Found cargo"
-else
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-    source $HOME/.cargo/env
-    export PATH=$HOME/.cargo/bin:$PATH
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 wasm-pack build --target web --out-name wasm --out-dir "$DIR/../static/out"
