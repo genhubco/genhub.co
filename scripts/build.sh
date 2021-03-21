dba8d34bf906300c8e17036f982c7c4c62e30c30
Filip <lazoviccorp@gmail.com>
2021-03-21 02:07:19 +0100

    tst

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 wasm-pack build --release --target web --out-name wasm --out-dir "$DIR/../static/out"
