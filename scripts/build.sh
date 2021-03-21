9a983ea4ae704fe9673887c5697e3d300df7df4a
Filip <lazoviccorp@gmail.com>
2021-03-21 02:05:58 +0100

    Release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-wasm-pack build --target web --out-name wasm --out-dir "$DIR/../static/out"
+wasm-pack build --release --target web --out-name wasm --out-dir "$DIR/../static/out"
