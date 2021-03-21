8c33d48c2edc0685206c05ec53c061b2762313d8
Filip <lazoviccorp@gmail.com>
2021-03-21 02:08:53 +0100

    tst 2

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,11 @@
+if [[ $(cargo --version) ]]; then
+    echo "Found cargo"
+else
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+    export PATH=$HOME/.cargo/bin:$PATH
+fi
+
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
